1e2c9232a628b88673f08579742edf6269e3550c
stan <119288936+mosetf@users.noreply.github.com>
2024-01-04 14:46:34 +0300

    Fix Linting Error

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize = 0) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
-}
+  constructor(maxStudentsSize = 0) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+} 
