50ec568c1a3b1fffa52f00a17b8bf321c98fb51c
mosetf <stanlym315@gmail.com>
2024-01-08 23:10:47 +0300

    returns an array of objects who are located in a specific city.

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(students, city) {
+    if (students instanceof Array) {
+      return students.filter((student) => student.location === city);
+    }
+    return [];
+  }
+  
