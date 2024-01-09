2ac44e434872ecb3b308d0ea552bd475a7713322
mosetf <stanlym315@gmail.com>
2024-01-09 23:18:16 +0300

    returns an array of ids from a list of object

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,9 @@
 export default function getListStudentIds(students) {
-    if (students instanceof Array) {
-      return students.map((student) => student.id);
-    }
-    return [];
+  // Check if students is an instance of Array
+  if (students instanceof Array) {
+    // If true, use map to extract the "id" property from each object
+    return students.map((student) => student.id);
   }
+  // If students is not an array, return an empty array
+  return [];
+}
