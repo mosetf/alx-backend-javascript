e423d620f1231c4e57f5711c3feadf707a7147f6
mosetf <stanlym315@gmail.com>
2024-01-09 23:48:02 +0300

    checks if all values in an array are present in a Set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,5 @@
 export default function hasValuesFromArray(set, array) {
-    return array.every((value) => set.has(value));
-  }
+  // Use the every function to check if every value
+  // in the array is present in the Set
+  return array.every((value) => set.has(value));
+}
