04ec4d0f766489e54d77c5a6307d4440d5a3ffb8
mosetf <stanlym315@gmail.com>
2024-01-09 23:51:38 +0300

    creates a Map representing a grocery list where each item is associated with a quantity

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,13 @@
 export default function groceriesList() {
-    const values = [
-      ['Apples', 10],
-      ['Tomatoes', 10],
-      ['Pasta', 1],
-      ['Rice', 1],
-      ['Banana', 5],
-    ];
-    return new Map(values);
-  }
+  // Array of arrays containing items and their respective quantities
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  // Create a Map using the provided values
+  return new Map(values);
+}
