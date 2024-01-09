d6deaa1f0bf212c78a3d33d701b2b2f67e85f496
mosetf <stanlym315@gmail.com>
2024-01-09 23:56:10 +0300

    updates the values in a Map by setting them to 100 if the original value is 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,16 @@
 export default function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-      throw new Error('Cannot process');
-    }
-    map.forEach((value, key) => {
-      if (value === 1) {
-        map.set(key, 100);
-      }
-    });
+  // Check if the input is a Map
+  if (!(map instanceof Map)) {
+    // If not, throw an error
+    throw new Error('Cannot process');
   }
+
+  // Iterate over each key-value pair in the Map
+  map.forEach((value, key) => {
+    // Check if the value is equal to 1
+    if (value === 1) {
+      // If true, update the value to 100
+      map.set(key, 100);
+    }
+  });
+}
