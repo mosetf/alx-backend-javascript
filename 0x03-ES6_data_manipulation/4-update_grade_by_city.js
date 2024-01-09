23521e7c8a0236e01a07205a4f4965b56e243ae0
mosetf <stanlym315@gmail.com>
2024-01-09 23:36:33 +0300

    eturns an array of students for a specific city with their new grade accepting a list of students, a city and newgrades as param

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,24 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    const defaultGrade = { grade: 'N/A' };
-  
-    if (students instanceof Array) {
-      return students
-        .filter((student) => student.location === city)
-        .map((student) => ({
-          id: student.id,
-          firstName: student.firstName,
-          location: student.location,
-          grade: (newGrades
-            .filter((grade) => grade.studentId === student.id)
-            .pop() || defaultGrade).grade,
-        }));
-    }
-    return [];
+  // Define a default grade object
+  const defaultGrade = { grade: 'N/A' };
+
+  // Check if students is an instance of Array
+  if (students instanceof Array) {
+    // Use filter to get students in the specified city
+    // Use map to create a new array with updated grades
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        // Use filter and pop to get the latest grade for the student
+        // If no grade is found, use the default grade
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
   }
+  // If students is not an array, return an empty array
+  return [];
+}
