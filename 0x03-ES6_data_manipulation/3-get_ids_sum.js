2903fbf3584d79a8eca6589845124c8b1f007757
mosetf <stanlym315@gmail.com>
2024-01-09 23:31:48 +0300

    returns the sum of all the student ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,9 +1,9 @@
 export default function getStudentIdsSum(students) {
-    if (students instanceof Array) {
-      return students.reduce(
-        (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
-        0,
-      );
-    }
-    return 0;
+  // Check if students is an instance of Array
+  if (students instanceof Array) {
+    // Use reduce to calculate the sum of student IDs
+    return students.reduce((sum, student) => sum + student.id, 0);
   }
+  // If students is not an array, return 0
+  return 0;
+}
