40b2cdcd2cfcf4ca96ca698a0dc39db6c0d8b102
mosetf <stanlym315@gmail.com>
2024-01-10 17:54:03 +0300

    Creating functions specific to employees and fix lint error

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,50 +1,55 @@
 interface DirectorInterface {
   workFromHome(): string;
+
   getCoffeeBreak(): string;
+
   workDirectorTasks(): string;
 }
 
 interface TeacherInterface {
   workFromHome(): string;
+
   getCoffeeBreak(): string;
+
   workTeacherTasks(): string;
 }
 
 // Implement the Director class
-class Director implements DirectorInterface {
-  workFromHome(): string {
-    return "Working from home";
-  }
+export class Director implements DirectorInterface {
 
-  getCoffeeBreak(): string {
-    return "Getting a coffee break";
-  }
+  getCoffeeBreak = (): string => "Getting a coffee break";
 
-  workDirectorTasks(): string {
-    return "Getting to director tasks";
-  }
+  workDirectorTasks = (): string => "Getting to director tasks";
+
+  workFromHome = (): string => "Working from home";
+
 }
 
-// Implement the Teacher class
-class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return "Cannot work from home";
-  }
+export class Teacher implements TeacherInterface {
+  getCoffeeBreak = (): string => "Cannot have a break";
 
-  getCoffeeBreak(): string {
-    return "Cannot have a break";
-  }
+  workFromHome = (): string => "Cannot work from home";
 
-  workTeacherTasks(): string {
-    return "Getting to work";
-  }
+  workTeacherTasks = (): string => "Getting to work";
+
 }
 
-// Create the createEmployee function
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
-    return new Teacher();
-  } else {
-    return new Director();
-  }
+export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
+
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+  return (employee as Director).workDirectorTasks() !== undefined;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  let res = undefined;
+  (isDirector(employee)) ? res = employee.workDirectorTasks() : res = employee.workTeacherTasks();
+  return res;
+}
+
+type Subjects = "Math" | "History";
+
+// @ts-ignore
+// @ts-ignore
+export function teachClass(todayClass: Subjects): string | boolean {
+  return (todayClass === "Math") ? "Teaching Math" : (todayClass === "History") ? "Teaching History" : `${undefined}`
 }
