ae51a21146e4635a76789e5794b738ed24633932
mosetf <stanlym315@gmail.com>
2024-01-10 17:09:23 +0300

    Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    readonly fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    readonly location: string;
+    [key: string]: boolean | number | string; // Allow any additional attribute
+  }
