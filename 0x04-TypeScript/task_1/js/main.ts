1786b4fd828553caa3eb36d823a61995f6733003
mosetf <stanlym315@gmail.com>
2024-01-10 17:24:00 +0300

    DirectorInterface, TeacherInterface , class Director, Teacher and funtion employee that checks if one is a Teacher or employee

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,48 +1,41 @@
-interface Teacher {
-    firstName: string;
-    lastName: string;
-    readonly fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    readonly location: string;
-    [key: string]: boolean | number | string; // Allow any additional attribute
-  }
-  interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
   }
   
-  // Implement the printTeacher function
-  const printTeacher: printTeacherFunction = (firstName, lastName) => {
-    const firstLetter = firstName.charAt(0).toUpperCase();
-    const fullLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
-  
-    return `${firstLetter}. ${fullLastName}`;
-  };
-
-  interface StudentConstructor {
-    firstName: string;
-    lastName: string;
+  interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
   }
   
-  interface StudentClassInterface {
-    workOnHomework(): string;
-    displayName(): string;
-  }
-  
-  // Implement the StudentClass
-  class StudentClass implements StudentClassInterface {
-    private firstName: string;
-    private lastName: string;
-  
-    constructor({ firstName, lastName }: StudentConstructor) {
-      this.firstName = firstName;
-      this.lastName = lastName;
+  // Implement the Director class
+  class Director implements DirectorInterface {
+    workFromHome(): string {
+      return "Working from home";
     }
   
-    workOnHomework(): string {
-      return "Currently working";
+    getCoffeeBreak(): string {
+      return "Getting a coffee break";
     }
   
-    displayName(): string {
-      return this.firstName;
+    workDirectorTasks(): string {
+      return "Getting to director tasks";
+    }
+  }
+  
+  // Implement the Teacher class
+  class Teacher implements TeacherInterface {
+    workFromHome(): string {
+      return "Cannot work from home";
+    }
+  
+    getCoffeeBreak(): string {
+      return "Cannot have a break";
+    }
+  
+    workTeacherTasks(): string {
+      return "Getting to work";
     }
   }
