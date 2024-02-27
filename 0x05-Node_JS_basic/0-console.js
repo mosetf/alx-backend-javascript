6e80f77f7c1d297ab77baf9ec65c65cad432acb7
mosetf <stanlym315@gmail.com>
2024-02-27 16:09:49 +0300

    .\.eslintrc.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * prints message in STDOUT
+ * @param {*} message
+ */
+function displayMessage(message) {
+  //dispalys message in STDOUT
+  console.log(message);
+}
+module.exports = displayMessage;
