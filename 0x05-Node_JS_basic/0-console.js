fac782af2c90c50bc6b0a392bf9bb60029482929
mosetf <stanlym315@gmail.com>
2024-02-27 17:25:19 +0300

    fix 0.conlole

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,8 @@
  * prints message in STDOUT
  * @param {*} message
  */
-function displayMessage(message) {
-  //dispalys message in STDOUT
+function displayMessage (message) {
+  // dispalys message in STDOUT
   console.log(message);
 }
 module.exports = displayMessage;
